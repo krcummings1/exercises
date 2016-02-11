369c1d8e22ec0dbb476254c5dbcbcb47ad0c4cf4
Kaylee Cummings <kayleecummings@Kaylees-MacBook-Pro.local>
2016-02-11 12:54:23 -0600

    output coins to DOM

--- the-static-web/converter/converter.js
+++ the-static-web/converter/converter.js
@@ -16,64 +16,54 @@ var button = document.getElementById("button");
 var result = document.getElementById("result");
 
 button.addEventListener("click", coinCounter);
+// ^ says when button is "click"ed, run coinCounter function
+// this is how I'm calling this function, based off of the "click" event on the button
 
-// first, I need to write out how many quarters I need based off of the input value, so for 80, 80/25 = 3.2  I would have 3 quarters equaling 75, then 80-75 = 5
-
-// then, I need to take the remaining value (5) and determine how many dimes.... the remaining value from above is 5, so I need 0 dimes... moving on to nickels....
-// then, taking the remaining value and move on to nickels and pennies.... the remaining value is 5 so I need 1 nickel 
-
-// Now, I need to count the number for each coin 
-
-// Then output the coins 
-var coinPurse = {};
+var coinPurse = {
+  "quarters": 0,
+  "dimes": 0,
+  "nickels": 0,
+  "pennies": 0,
+};
+// originally had an empty object, but set it up with keys and values to set the values to zero
+// if there were no dimes, it was coming back as undefined because I didn't give it a base value
 
 function coinCounter () {
   var remainder;
-
   coinPurse.quarters = Math.floor(input.value / 0.25);
   remainder = (input.value - (coinPurse.quarters*0.25)).toFixed(2);
   console.log(remainder);
   console.log("quarters", coinPurse.quarters);
-
   if (remainder < 0.25 && remainder >= 0.10) {
     coinPurse.dimes = Math.floor(remainder / 0.10);
     remainder = (remainder - (coinPurse.dimes*0.10)).toFixed(2);
-      console.log(remainder);
-      console.log("dimes", coinPurse.dimes);
-
-      if (remainder < 0.10 && remainder >= 0.05) {
+    console.log(remainder);
+    console.log("dimes", coinPurse.dimes);
+    }
+  if (remainder < 0.10 && remainder >= 0.05) {
     coinPurse.nickels = Math.floor(remainder / 0.05);
     remainder = (remainder - (coinPurse.nickels*0.05)).toFixed(2);
-      console.log(remainder);
-      console.log("nickels", coinPurse.nickels);
-
-      if (remainder < 0.05 && remainder >= 0.01) {
+    console.log(remainder);
+    console.log("nickels", coinPurse.nickels);
+    }
+  if (remainder < 0.05 && remainder >= 0.01) {
     coinPurse.pennies = (remainder / 0.01);
-      console.log(remainder);
-      console.log("pennies", coinPurse.pennies);
+    console.log(remainder);
+    console.log("pennies", coinPurse.pennies);
     }
-    }
-  // else if (remainder < 0.10 && remainder >= 0.05) {
-  //   coinPurse.nickels = Math.floor(remainder / 0.05);
-  //   remainder = (remainder - (coinPurse.nickels*0.05));
-  //     console.log(remainder);
-  //     console.log("nickels", coinPurse.nickels);
-  //   }
-  // else if (remainder < 0.05 && remainder >= 0.01) {
-  //   coinPurse.pennies = Math.floor(remainder / 0.01);
-  //   remainder = (remainder - (coinPurse.pennies*0.01));
-  //     console.log(remainder);
-  //     console.log("pennies", coinPurse.pennies);
-  }
-  };
+    console.log("coinPurse object", coinPurse);
+    outputToDOM();
+};
 
-coinCounter();
-console.log(coinPurse);
 
-// console.log(button.addEventListener("click", coinCounter), coinPurse);
-
-// var coins = coinCounter()
-// console.log();
+function outputToDOM(){
+  console.log(coinPurse);
+  result.innerHTML = `<div>Quarters: ${coinPurse.quarters}</div>`
+  + `<div>Dimes: ${coinPurse.dimes}</div>`
+  + `<div>Nickels: ${coinPurse.nickels}</div>`
+  +`<div>Pennies: ${coinPurse.pennies}</div>`;
+};
+// object shows up in console, but can't output just an object to the DOM, I have to set it up with the keys and values (data within)
 
 
 
